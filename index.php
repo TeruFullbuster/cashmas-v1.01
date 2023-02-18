4ab29e5fef33de2918ed17bc856d56a1a26b151d
Angel Ruiz <terufullbustee@gmail.com>
2023-02-18 16:29:55 -0600

    Updates

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 </head>
+
 <body>
 	<section class="sec-center">
 		<img src="img/cashmas-logo.svg" alt="logo cashmas" class="img-center">
